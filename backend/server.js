d6d6ba92249b3c22bbd264d8859d6cb661593591
yogesh-u-k <yogeshkumbhar1503@gmail.com>
2024-06-19 19:37:07 +0530

    1- notification routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import { v2 as cloudinary } from "cloudinary";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
+import notificationRoutes from "./routes/notification.route.js";
 import dotenv from "dotenv";
 import connectMongodb from "./db/connectMongodb.js";
 import cookieParser from "cookie-parser";
@@ -25,6 +26,8 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/notifications",notificationRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
