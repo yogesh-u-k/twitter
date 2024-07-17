f68a686efa40676c04c8bf1ed3db4191f0d68eed
yogesh-u-k <yogeshkumbhar1503@gmail.com>
2024-07-18 00:22:02 +0530

    3- userFollow hook Added

--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
 import { USERS_FOR_RIGHT_PANEL } from "../../uitils/db/dummy";
 import {useQuery} from "@tanstack/react-query"
+import useFollow from "../../hooks/useFollow";
+import {LoadingSpinner} from './LoadingSpinner'
 
 const RightPanel = () => {
 
@@ -20,6 +22,8 @@ const RightPanel = () => {
 			}
 		}
 	})
+
+	const {follow, isPending} = useFollow()
 
 	if(suggestedUsers?.length === 0)return <div className="md:w-64 w-0"></div>
 	return (
@@ -59,9 +63,12 @@ const RightPanel = () => {
 								<div>
 									<button
 										className='btn bg-white text-black hover:bg-white hover:opacity-90 rounded-full btn-sm'
-										onClick={(e) => e.preventDefault()}
+										onClick={(e) =>{
+											e.preventDefault()
+											follow(user._id)
+										}}
 									>
-										Follow
+										{isLoading ? <LoadingSpinner size='sm'/> : "Follow"}
 									</button>
 								</div>
 							</Link>
---
+++ frontend/src/hooks/useFollow.jsx
@@ -0,0 +1,32 @@
+import {useMutation, useQueryClient} from "@tanstack/react-query";
+import {toast} from "react-toastify";
+
+const useFollow = () => {
+    const queryClient = useQueryClient();
+    const { mutate:follow, isPending} = useMutation({
+        mutationFn: async (userid) => {
+            const res = await fetch(`/api/users/follow/${userid}`, {
+                method: "POST",
+            });
+            const data = await res.json();
+            if (!res.ok) throw new Error(data.message);
+            return data;
+        },          
+        onSuccess: () => {  
+            
+            Promise.all([
+                queryClient.invalidateQueries({ queryKey: ["suggestedUsers"] }),
+            queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+            ])
+            
+            
+        },
+        onError: (error) => {   
+            toast.error(error.message);
+        },
+    });
+
+    return {follow, isPending}
+}
+
+export default useFollow;
