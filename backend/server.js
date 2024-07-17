d9f80444fb4c93baf4f14d1101f0cd1d28166037
yogesh-u-k <yogeshkumbhar1503@gmail.com>
2024-07-18 00:05:47 +0530

    3- Suggested users fetched

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const app = express();
 console.log(process.env.MONGOURI);
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json({limit:"5mb"}));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -1,10 +1,27 @@
 import { Link } from "react-router-dom";
 import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
 import { USERS_FOR_RIGHT_PANEL } from "../../uitils/db/dummy";
+import {useQuery} from "@tanstack/react-query"
 
 const RightPanel = () => {
-	const isLoading = false;
 
+	const {data:suggestedUsers, isLoading} = useQuery({	
+		queryKey: ["suggestedUsers"],
+		queryFn: async()=>{	
+			try{
+				const res = await fetch('/api/users/suggested')
+				const data = await res.json()
+				if(!res.ok) throw new Error(data.error || 'Failed to fetch suggested users')
+				return data
+			}
+			catch(error){
+				console.log(error)
+				throw error
+			}
+		}
+	})
+
+	if(suggestedUsers?.length === 0)return <div className="md:w-64 w-0"></div>
 	return (
 		<div className='hidden lg:block my-4 mx-2'>
 			<div className='bg-[#16181C] p-4 rounded-md sticky top-2'>
@@ -20,7 +37,7 @@ const RightPanel = () => {
 						</>
 					)}
 					{!isLoading &&
-						USERS_FOR_RIGHT_PANEL?.map((user) => (
+						suggestedUsers?.map((user) => (
 							<Link
 								to={`/profile/${user.username}`}
 								className='flex items-center justify-between gap-4'
@@ -34,7 +51,7 @@ const RightPanel = () => {
 									</div>
 									<div className='flex flex-col'>
 										<span className='font-semibold tracking-tight truncate w-28'>
-											{user.fullName}
+											{user.fullname}
 										</span>
 										<span className='text-sm text-slate-500'>@{user.username}</span>
 									</div>
